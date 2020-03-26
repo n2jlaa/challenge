9877649395271e05e43166d29e95dc0fe830db64
Najlaa Alahmari <najlaasafar110@gmail.com>
2020-03-26 13:01:39 +0300

    updates

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,15 +1,54 @@
 const router = require("express").Router();
+const User = require("../models/user.model");
 const passport = require("../config/passportConfig");
 const isLoggedIn = require("../config/loginBlocker");
+const { check, validationResult } = require('express-validator');
+
+router.get("/auth/signup", (request, response) => {
+  response.render("auth/signup");
+});
+
+router.get("/auth/signin", (request, response) => {
+  response.render("auth/signin");
+});
+
+router.post("/auth/signup", [
+      check('name').isLength({ min: 3 }),
+      check('phone').isLength({ min: 3 }),
+      check('email').isEmail(),
+      check('password').isLength({ min: 6 })
+  ],
+  (request, response) => {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+          request.flash("autherror", errors.errors)
+          return response.redirect("/auth/signup");
+      }
+      let user = new User(request.body);
+      user.save()
+          .then(() => {
+              // response.redirect("/home");
+              passport.authenticate("local", {
+                  successRedirect: "/home",
+                  successFlash: "Account created and Logged In!"
+              })(request, response);
+          })
+          .catch(err => {
+              console.log(err);
+              request.flash("error", "Email already exists!");
+              return response.redirect("/auth/signup");
+          });
+  }
+);
 
 //-- Login Route
 router.post(
   "/auth/signin",
   passport.authenticate("local", {
-    successRedirect: "/home", //after login success
-    failureRedirect: "/auth/signin", //if fail
-    failureFlash: "Invalid Username or Password",
-    successFlash: "You have logged In!"
+      successRedirect: "/home", //after login success
+      failureRedirect: "/auth/signin", //if fail
+      failureFlash: "Invalid Username or Password",
+      successFlash: "You have logged In!"
   })
 );
 
@@ -19,3 +58,5 @@ router.get("/auth/logout", (request, response) => {
   request.flash("success", "Dont leave please come back!");
   response.redirect("/auth/signin");
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
+require("dotenv").config();
 const express = require("express");
-const server = express();
+const PORT = process.env.PORT;
 const session = require("express-session");
-/*
+let passport = require("./config/passportConfig");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const authRoutes = require("./routes/auth.route");
+const server = express();
 
-*/
+mongoose.connect(
+  process.env.MONGODB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  () => {
+    console.log("mongdb connected!");
+  },
+  err => {
+    console.log(err);
+  }
+);
+
+mongoose.set("debug", true);
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.set("view engine", "ejs");
+server.use(expressLayouts);
 
 /*-- These must be place in the correct place */
 server.use(
@@ -11,7 +36,7 @@ server.use(
     secret: process.env.SECRET,
     saveUninitialized: true,
     resave: false,
-    cookie: { maxAge: 360000 }
+    // cookie: { maxAge: 360000 }
   })
 );
 //-- passport initialization
@@ -26,6 +51,13 @@ server.use(function(request, response, next) {
   next();
 });
 
+server.use(authRoutes);
+
+server.get("*", (request, response) => {
+  response.send("doesnt exist yet!");
+});
+
 server.listen(process.env.PORT, () =>
   console.log(`connected to express on ${PORT}`)
 );
+
